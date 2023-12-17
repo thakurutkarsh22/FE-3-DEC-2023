fc730dee890cddea6f3946776a7b34f662d7b194
utkarshCf <utkarsh.thakur@curefit.com>
2023-12-17 15:37:17 +0530

    session 5

---
+++ session1/index.js
@@ -0,0 +1,19 @@
+console.log("hello world js");
+
+const headingElement = document.getElementById("heading");
+headingElement.textContent = "abcd";
+console.dir(headingElement);
+
+const headingElement1 = document.getElementsByTagName("h1");
+console.dir(headingElement1);
+
+const headingClassElement = document.getElementsByClassName("heading"); // HTML Collection
+console.dir(headingClassElement);
+
+const querySelectorHeadingElement = document.querySelector("");
+
+const querySelectorAll = document.querySelectorAll(); // Nodes List
+
+// console.log("helloow ");
+
+console.log;
