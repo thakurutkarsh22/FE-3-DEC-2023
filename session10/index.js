c3ffd616e68ba5e69f8aa2afaba145e772dea316
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2023-12-26 22:46:06 +0530

    linux commands

---
+++ session10/index.js
@@ -0,0 +1,31 @@
+// -------- MANUAL OF ANY COMMAND
+// man ls
+// man cd
+// man mkdir
+
+// ------- Clear HISTORY
+// clear
+
+// ------ Directory Commands
+
+// 1. mkdir - creats a directory
+// 2. pwd - print working directory - in which FOLDER (DIRECTORY) you are working.
+// 3. cd - change directory - to go from one FOLDER (DIRECTORY) to ANOTHER.
+// 4. ls - list down - to list all the elements inside that Perticular FOLDER (DIRECTORY).
+// 5. cd .. - take you to the parent
+// 6. rmdir - removes empty directory
+// 7. rm -r folderName - remove the non empty directory also.
+
+// ------- FILES
+// touch - creates a file.
+// cat fileName - content inside the file.
+// vim fileName - edit the file.
+// cp fileName destination - copy the file in destination
+// mv filename destination
+
+// destination can be RELATIVE PATH or ABSOLUTE PATH (/anypath)
+
+// ------ Argument Vs Options
+
+// cd criodec-2 -> this is argument
+// ls  -l -> -l is options. (you can always check all the options via MANUAL PAGE)
